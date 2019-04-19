63c3f8fecb2e5bda246b30acf72b51b42dfa584f
dannykrug <dannykrug@gmail.com>
2019-04-18 21:35:54 -0400

    added about

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default class About extends Component {
   render() {
   return (
     <div>
-    About Danny
+    I'm a full stack web developer with a passion for solving everyday problems with intuitive solutions. With experience in Ruby on Rails, JavaScript, and React and a background in photography and music, I discovered web development through my own desire for simpler solutions to problems in my previous fields of work. I bring strong skills in team-building, design and project management that help companies improve their overall user experience.
     </div>
   )}
 }
