3247eb0eb19969c6e12f017ff9501a69d24d538c
dannykrug <dannykrug@gmail.com>
2019-04-18 21:59:36 -0400

    background images?

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './LandingPage.css'
 
 
 export default class LandingPage extends Component {
   render() {
     return(
-      <div>
+      <div class='landing-wrapper'>
+      <div class='bg-image'>
+      <div class='container'>
         <h1> Danny Krug </h1>
         <h2> Full Stack Developer </h2>
-        
+
         <div>
           <Link to="/about">About</Link>
         </div>
         <div>
           <Link to="/contact">Contact</Link>
         </div>
+      </div>
+      </div>
       </div>
     )
   }
